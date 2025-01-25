65ea6d059d1bbe89d32ee692cc9e502a3e3f2cb2
mohshaneh <m.shanehsazzadeh@yahoo.com>
2025-01-25 21:51:23 +0330

    completing struct and before game function and finfing person

--- game.c
+++ game.c
@@ -5,9 +5,10 @@
 #include <string.h>
 #include <ctype.h>
 
+char **user;
+
 typedef struct
 {
-    int line;
     int gold;
     int life_time;
     int hungry_amount;
@@ -15,10 +16,35 @@ typedef struct
     int selah[20];
     int x;
     int y;
-} player;
+} Player;
+
+Player player;
+
+void which_user(int line_user)
+{
+    FILE *file_account_password;
+    file_account_password = fopen("accounts_passwords.txt", "r");
+    char line[300];
+
+    user = (char **) malloc(3 * sizeof(char *));
+    for (int j = 0; j < 3; j++)
+    {
+        *(user + j) = (char *) malloc(100*sizeof(char));
+    }
+
+    do
+    {
+        fgets(line, sizeof(line), file_account_password);
+        line[strcspn(line, "\n")] = '\0';
+        sscanf(line, "%s %s %s", user[0], user[1], user[2] );
+        line_user--;
+    } while (line_user != 0);
+}
 
 int before_play_menu(int line)
 {
+    clear_and_border();
+    which_user(line);
 
 }
 
